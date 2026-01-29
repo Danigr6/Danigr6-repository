82dd861ea7dfeb4597963f858ab12764c0e3743e
Sistemas <plusdaniel.garcia7@gmail.com>
2026-01-29 19:09:07 +0100

    Creacion de clase usuario y creacion de metodo arrayUsuarios

--- Principal.java
+++ Principal.java
@@ -1,16 +1,40 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Principal {
-	public static void main(String [] args) {
-		System.out.println("Hola mundo");
-		System.out.println("Fecha y hora actual: " + fechaHoraActual());
+	public static Scanner teclado = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int cantUsuarios = 3;
+		Usuario[] usuarios = arrayUsuarios(cantUsuarios);
+		
+		System.out.println("\n---- LISTA DE USUARIOS ----");
+		for (int i = 0; i < usuarios.length; i++) {
+			System.out.println("Nombre: " + usuarios[i].nombre);
+            System.out.println("Apellidos: " + usuarios[i].apellidos);
+            System.out.println("Email: " + usuarios[i].email);
+            System.out.println("------------------------");
+		}
+
 	}
 
-	public static String fechaHoraActual() {
-		LocalDateTime ahora = LocalDateTime.now();
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        
-        return ahora.format(formato);
+	public static Usuario[] arrayUsuarios(int cantidad) {
+		Usuario[] usuarios = new Usuario[cantidad];
+		
+		for (int i = 0; i < cantidad; i++) {
+			usuarios[i] = new Usuario();
+			System.out.println("\n" + (i + 1) + "º Usuario");
+
+			System.out.print("Introduce nombre: ");
+			usuarios[i].nombre = teclado.nextLine().trim();
+
+			System.out.print("Apellidos: ");
+			usuarios[i].apellidos = teclado.nextLine().trim();
+
+			System.out.print("Email: ");
+			usuarios[i].email = teclado.nextLine().trim();
+		}
+
+		return usuarios;
 	}
+
 }
