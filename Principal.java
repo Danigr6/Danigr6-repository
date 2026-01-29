7f24b375ea08cb84a3036005e0df4cf0017d7b8a
Sistemas <plusdaniel.garcia7@gmail.com>
2026-01-29 18:23:32 +0100

    Hola mundo con fecha y hora actual

--- Principal.java
+++ Principal.java
@@ -1,5 +1,16 @@
-public class Principal{
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Principal {
 	public static void main(String [] args) {
-		System.out.print("Hola mundo");
+		System.out.println("Hola mundo");
+		System.out.println("Fecha y hora actual: " + fechaHoraActual());
+	}
+
+	public static String fechaHoraActual() {
+		LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        
+        return ahora.format(formato);
 	}
 }
